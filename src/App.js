9a35bd7d96f2004dcf2c93310f941dbe3fe3f96b
JOPI-RTX3060\johan <johannes@walenta.dev>
2022-12-09 11:22:47 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from "three";
+import { extend } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, shaderMaterial } from "@react-three/drei";
+import glsl from 'babel-plugin-glsl/macro';
+
+const ColorShiftMaterial = shaderMaterial(
+  { time: 0, color: new THREE.Color(0.2, 0.0, 0.1) },
+  // vertex shader
+  glsl`
+    varying vec2 vUv;
+    
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  // fragment shader
+  glsl`
+    uniform float time;
+    uniform vec3 color;
+    varying vec2 vUv;
+    void main() {
+      gl_FragColor.rgba = vec4(0.5 + 0.3 * sin(vUv.yxx + time) + color, 1.0);
+    }
+  `
+)
+
+extend({ ColorShiftMaterial });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <ambientLight intensity={1} color="blue" />
+
+      <mesh>
+        <boxGeometry />
+        <colorShiftMaterial color="hotpink" time={2} />
+      </mesh>
+
+      <OrbitControls />
+    </Canvas>
   );
 }
 
